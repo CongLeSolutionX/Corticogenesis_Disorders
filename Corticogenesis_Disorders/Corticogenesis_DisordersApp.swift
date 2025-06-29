0795516885dfbeb84e3211baf066d69e66e97c1d
Cong Le <longchike@gmail.com>
2025-06-29 02:01:12 -0700

    The setup

---
+++ Corticogenesis_Disorders/Corticogenesis_DisordersApp.swift
@@ -0,0 +1,17 @@
+//
+//  Corticogenesis_DisordersApp.swift
+//  Corticogenesis_Disorders
+//
+//  Created by Cong Le on 6/29/25.
+//
+
+import SwiftUI
+
+@main
+struct Corticogenesis_DisordersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
